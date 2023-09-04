dcf6a0061fb2116ce99393d8bd849813556d5e53
Andreas Roth <andreas.roth@scs.ch>
2023-09-04 11:33:20 +0200

    Tested compilation of plugin-3 with esbuild (no success)

---
+++ projects/micro-plugin-3/src/app/three/three.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-three',
+  templateUrl: './three.component.html',
+  styleUrls: ['./three.component.css'],
+  standalone: true
+})
+export class ThreeComponent {
+
+}
